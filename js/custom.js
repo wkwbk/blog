61243d615156a60f9e65afbc9664e6c95ab6a8ef
wkwbk <lisir.me@outlook.com>
2024-01-05 19:23:55 +0800

    Site updated: 2024-01-05 19:23:54

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var script = document.createElement("script");
+script.setAttribute("type", "text/javascript");
+script.setAttribute("id", "myhk");
+script.setAttribute("src", "https://myhkw.cn/api/player/163862811020");
+script.setAttribute("key", "163862811020");
+script.setAttribute("m", "1");
+document.documentElement.appendChild(script);
