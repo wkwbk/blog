1d777860869c0ad74cf6c7009e41bbadeebf011c
wkwbk <lisir.me@outlook.com>
2024-01-11 23:54:25 +0800

    Site updated: 2024-01-11 23:54:25

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// 导入明月浩空播放器
 var script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.setAttribute("id", "myhk");
@@ -5,3 +6,8 @@ script.setAttribute("src", "https://myhkw.cn/api/player/163862811020");
 script.setAttribute("key", "163862811020");
 script.setAttribute("m", "1");
 document.documentElement.appendChild(script);
+
+// 禁用右键菜单
+document.oncontextmenu = function (e) {
+  return false;
+};
